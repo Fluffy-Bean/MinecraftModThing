229e4a8a3bcab5dc36f4acde3201fed844fe641e
Fluffy-Bean <michal-gdula@protonmail.com>
2024-01-11 17:02:10 +0000

    Initial commit

---
+++ src/main/java/gay/balls/templatemod/TemplateModClient.java
@@ -0,0 +1,10 @@
+package gay.balls.templatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/gay/balls/templatemod/TemplateModDataGenerator.java
@@ -0,0 +1,11 @@
+package gay.balls.templatemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
